11cfe6ba27400711ae8bbc0bb5bfd33a336c732d
RonniE997 <47603890+RonniE997@users.noreply.github.com>
2020-04-14 15:30:39 +0200

    prova commit

--- ProjectX/app/src/main/java/com/example/projectx/MainActivity.java
+++ ProjectX/app/src/main/java/com/example/projectx/MainActivity.java
@@ -15,5 +15,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         recyclerView = findViewById(R.id.recyclerView);
+        //copate
     }
 }
