ae834642489ea4559ad65982899f0efef1c445ed
RonniE997 <47603890+RonniE997@users.noreply.github.com>
2020-04-14 17:55:42 +0200

    aggiunto il detail layout e fatto il layout

--- ProjectX/app/src/main/java/com/example/projectx/DetailActivity.java
+++ ProjectX/app/src/main/java/com/example/projectx/DetailActivity.java
@@ -1,20 +1,14 @@
 package com.example.projectx;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
-
-    RecyclerView recyclerView;
+public class DetailActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
-
-        recyclerView = findViewById(R.id.recyclerView);
-
     }
 }
