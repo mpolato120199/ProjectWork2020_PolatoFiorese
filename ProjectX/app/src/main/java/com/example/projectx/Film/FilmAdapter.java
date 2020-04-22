0d44b0a58c39b99a97b270b3a921dd99c29d4bbc
RonniE997 <47603890+RonniE997@users.noreply.github.com>
2020-04-22 10:48:14 +0200

    Aggiungo Classi

--- ProjectX/app/src/main/java/com/example/projectx/Film/FilmAdapter.java
+++ ProjectX/app/src/main/java/com/example/projectx/Film/FilmAdapter.java
@@ -1,4 +1,4 @@
-package com.example.projectx.adapter;
+package com.example.projectx.Film;
 
 import android.content.Context;
 import android.content.Intent;
@@ -6,7 +6,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
---
+++ ProjectX/app/src/main/java/com/example/projectx/Film/FilmDB.java
@@ -0,0 +1,4 @@
+package com.example.projectx.Film;
+
+public class FilmDB {
+}
