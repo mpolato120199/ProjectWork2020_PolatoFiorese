92ddf6c368461077548044acec8444ae5dace2a0
mpolato120199 <mpolato120199@hotmail.com>
2020-04-27 12:37:34 +0200

    layout definitivo e API in italiano

--- ProjectX/app/src/main/java/com/example/projectx/data/services/FilmService.java
+++ ProjectX/app/src/main/java/com/example/projectx/data/services/FilmService.java
@@ -12,7 +12,7 @@ public interface FilmService {
 // https://api.themoviedb.org/3/trending/movie/week?api_key=a979a7eb2578017177824cf553c182ef
 // controlla se cambiano durante la settimana l'ordine dei campi essendo trending della settimana
 
-    @GET("/3/trending/movie/week?api_key=a979a7eb2578017177824cf553c182ef")
+    @GET("/3/trending/movie/week?api_key=a979a7eb2578017177824cf553c182ef&language=it-IT")
     Call<FilmResponse> getFilms(
             @Query("api_key") String apiKey
     );
